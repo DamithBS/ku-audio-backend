0da6d46014e15dd401548ecbc11762351d9192bf
DamithBS <damithbuddika0420@gmail.com>
2025-01-03 13:36:00 +0530

    create backend repository and index.js file

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+
+
+
+app.listen(3000, () => {
+    console.log("server is runing...");    
+});
