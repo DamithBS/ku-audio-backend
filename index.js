254b31867082450760ffc9d96ef0022455ef81fb
DamithBS <damithbuddika0420@gmail.com>
2025-01-03 14:10:11 +0530

    create a connection mongoDB 🌍

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 
+const app = express();
+
+app.use(bodyParser.json());
+
+let mongourl = "mongodb+srv://Admin:1234@cluster0.erezo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(mongourl);
+
+const connection = mongoose.connection
+
+connection.once("open",() => {
+    console.log("Connection create successful Connect......");   
+})
+
+
 
 
 
